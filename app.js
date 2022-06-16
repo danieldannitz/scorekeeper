4e3225a62a874f315a5f6bacd9f2f98a399a9e9c
danieldannitz <danielgnlmenezes@gmail.com>
2022-06-16 11:57:51 -0300

    javascript structure done, maybe not

---
+++ app.js
@@ -0,0 +1,20 @@
+const p1Button = document.querySelector("#p1Button");
+p1Button.addEventListener("click", () => {
+  let randomNum = Math.random();
+  console.log(randomNum);
+});
+
+const p2Button = document.querySelector("#p2Button");
+p2Button.addEventListener("click", () => {
+  let randomNum = Math.random();
+  console.log(randomNum);
+});
+
+const resetButton = document.querySelector("#resetButton");
+resetButton.addEventListener("click", () => {
+  let randomNum = Math.random();
+  console.log(randomNum);
+});
+
+let p1Score = document.querySelector("#p1Score");
+let p2Score = document.querySelector("#p1Score");
